a848b8a1602479a10e1bd8521df4dba158136957
Cristiane Damacena <damacena.cris@gmail.com>
2021-07-10 21:28:19 -0300

    Day1

---
+++ Day1.js
@@ -0,0 +1,105 @@
+//### Comment Your JavaScript Code
+// This is an in-line comment.
+
+/*
+*This
+*is
+*an
+*multi-line
+*comment
+*/
+
+//### Declare JavaScript Variables
+var myName;
+
+//### Storing Values with the Assignment Operator
+
+var a;
+a = 7;
+
+//### Assigning the Value of One Variable to Another
+
+var a;
+a = 7;
+var b;
+b = a;
+
+//### Initializing Variables with the Assignment Operator
+
+var a = 9;
+
+//### Understanding Uninitialized Variables
+
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+
+a = 5;
+b = 10;
+c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//### Understanding Case Sensitivity in Variables
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//### Add Two Numbers with JavaScript
+
+var sum = 10 + 10;
+
+//### Subtract One Number from Another with JavaScript
+
+var difference = 45 - 33;
+
+//### Multiply Two Numbers with JavaScript
+
+var product = 8 * 10;
+
+//### Divide One Number by Another with JavaScript
+
+var quotient = 66 / 33;
+
+//### Increment a Number with JavaScript
+
+var myVar = 87;
+
+  // Only change code below this line
+myVar ++;
+
+//### Decrement a Number with JavaScript
+
+var myVar = 11;
+
+// Only change code below this line
+myVar --;
+
+//### Create Decimal Numbers with JavaScript
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 15.7;
+
+//### Multiply Two Decimals with JavaScript
+
+var product = 2.0 * 2.5;
+
+//### Divide One Decimal by Another with JavaScript
+
+var quotient = 4.4 / 2.0; // Change this line
+
+//### Finding a Remainder in JavaScript
+
