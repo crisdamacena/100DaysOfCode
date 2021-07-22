bc71e87bfd1fb3e9d97465a9a15d18c2229794e7
Cristiane Damacena <damacena.cris@gmail.com>
2021-07-21 23:01:27 -0300

    Day2

---
+++ Day2.js
@@ -0,0 +1,132 @@
+//### Finding a Remainder in JavaScript
+
+var remainder;
+
+remainder = 11%3;
+
+//### Compound Assignment With Augmented Addition
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+//### Compound Assignment With Augmented Subtraction
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+//### Compound Assignment With Augmented Multiplication
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+//### Compound Assignment With Augmented Division
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+//### Declare String Variables
+
+var myFirstName = "Cristiane";
+var myLastName = "Oliveira";
+
+//### Escaping Literal Quotes in Strings
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+//### Quoting Strings with Single Quotes
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//### Escape Sequences in Strings
+
+var myStr = 'FirstLine\n\t\\SecondLine\nThirdLine'; // Change this line
+
+//### Concatenating Strings with Plus Operator
+
+var myStr = "This is the start. " + "This is the end."; // Change this line
+
+//### Concatenating Strings with the Plus Equals Operator
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+//### Constructing Strings with Variables
+
+var myName = "Cristiane";
+var myStr = "My name is " + myName + "and I am well!";
+
+//### Appending Variables to Strings
+
+var someAdjective = "great!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+
+//### Find the Length of a String
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line
+
+lastNameLength = lastName.length;
+
+//### Use Bracket Notation to Find the First Character in a String
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+//### Understand String Immutability
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+//### Use Bracket Notation to Find the Nth Character in a String
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var thirdLetterOfLastName = lastName[2]; // Change this line
+
+//### Use Bracket Notation to Find the Last Character in a String
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+
+//### Use Bracket Notation to Find the Nth-to-Last Character in a String
