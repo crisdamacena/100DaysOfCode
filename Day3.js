386878233c63da9ec2fd76e9878faf201bbce130
Cristiane Damacena <damacena.cris@gmail.com>
2021-07-29 02:36:50 -0300

    Day3

---
+++ Day3.js
@@ -0,0 +1,90 @@
+//### Use Bracket Notation to Find the Nth-to-Last Character in a String
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+//### Word Blanks
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "My " + myNoun + " is " + myAdjective + " and it's " + myVerb + " very " + myAdverb + "."; // Change this line
+// Only change code above this line
+
+//### Store Multiple Values in one Variable using JavaScript Arrays
+
+// Only change code below this line
+var myArray = ["string", 0];
+
+//### Nest one Array within Another Array
+
+// Only change code below this line
+var myArray = [["Lula", 13],["Bolsonaro", 17]];
+
+//### Access Array Data with Indexes
+
+var myArray = [50,60,70];
+var myData = myArray[0];
+
+//### Modify Array Data With Indexes
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line
+
+myArray[0] = 45;
+
+//### Access Multi-Dimensional Arrays With Indexes
+
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+var myData = myArray[2][1];
+
+//### Manipulate Arrays With push()
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+
+myArray.push(["dog", 3]);
+
+//### Manipulate Arrays With pop()
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.pop();
+
+//### Manipulate Arrays With shift()
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.shift();
+
+//### Manipulate Arrays With unshift()
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+
+myArray.unshift(["Paul",35]);
+
+//### Shopping List
+
+var myList = [["Chocolate Bar", 15], ["Candy", 10], ["Lollipop", 5], ["Cake", 7], ["Bread", 20]];
+
+//### Write Reusable JavaScript with Functions
+
