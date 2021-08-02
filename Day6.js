7153e7c910e768b1d1923a1a4ef92e2b3741215a
Cristiane Damacena <damacena.cris@gmail.com>
2021-08-02 03:11:58 -0300

    Day6

---
+++ Day6.js
@@ -0,0 +1,187 @@
+//### Explore Differences Between the var and let Keywords
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
+
+//### Compare Scopes of the var and let Keywords
+
+function checkScope() {
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+
+//### Declare a Read-Only Variable with the const Keyword
+
+function printManyTimes(str) {
+
+  // Only change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for (let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // Only change code above this line
+
+}
+printManyTimes("freeCodeCamp");
+
+//### Mutate an Array Declared with const
+
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  let aux = [2, 5, 7];
+
+  for (let i = 0; i < s.length; i++){
+    s[i] = aux[i];
+  }
+  
+
+  // Only change code above this line
+}
+editInPlace();
+
+//### Prevent Object Mutation
+
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // Only change code below this line
+  
+  Object.freeze(MATH_CONSTANTS);
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+//### Use Arrow Functions to Write Concise Anonymous Functions
+
+const magic = () => {
+  return new Date();
+};
+
+//### Write Arrow Functions with Parameters
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+//### Set Default Parameters for Your Functions
+
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+
+//### Use the Rest Parameter with Function Parameters
+
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+}
+
+//### Use the Spread Operator to Evaluate Arrays In-Place
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
+
+//### Use Destructuring Assignment to Extract Values from Objects
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const { today, tomorrow } = HIGH_TEMPERATURES;
+
+// Only change code above this line
+
+//### Use Destructuring Assignment to Assign Variables from Objects
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+  
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
+// Only change code above this line
+
+//### Use Destructuring Assignment to Assign Variables from Nested Objects
+
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+  
+const { today: { low: lowToday, high: highToday }} = LOCAL_FORECAST;
+
+// Only change code above this line
+
+//### Use Destructuring Assignment to Assign Variables from Arrays
+
+let a = 8, b = 6;
+// Only change code below this line
+
+[a,b] = [b,a];
+
+//### Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [a, b, ...arr] = list; // Change this line
+  // Only change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+
+//### Use Destructuring Assignment to Pass an Object as a Function's Parameters
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = ({ max, min }) => (max + min) / 2.0;
+// Only change code above this line
+
+//### Create Strings using Template Literals
+
