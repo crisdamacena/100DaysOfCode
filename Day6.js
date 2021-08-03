88c541888f10cf5132a117b2f9ad1d35268f1a04
Cristiane Damacena <damacena.cris@gmail.com>
2021-08-02 23:48:55 -0300

    Day6

--- Day6.js
+++ Day6.js
@@ -185,3 +185,61 @@ const half = ({ max, min }) => (max + min) / 2.0;
 
 //### Create Strings using Template Literals
 
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  for(let i = 0; i < arr.length; i++){
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+//### Write Concise Object Literal Declarations Using Object Property Shorthand
+
+const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  return {
+    name, age, gender
+  };
+  // Only change code above this line
+};
+
+//### Write Concise Declarative Functions with ES6
+
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+//### Use class Syntax to Define a Constructor Function
+
+// Only change code below this line
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
+
+//### Use getters and setters to Control Access to an Object
+
+
+
